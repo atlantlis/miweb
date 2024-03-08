748f24b31722a6edf8ebc810811ae9c554b17acd
Diana ARRUBLA <resources1@skiff.com>
2024-03-08 15:59:52 -0500

    Saludo personalizado

---
+++ archivo3.py
@@ -0,0 +1,3 @@
+nombre = input("Ingrese su nombre: ")  # Solicitar al usuario que ingrese su nombre
+mensaje = "¡Hola, " + nombre + "! Bienvenido."  # Construir el mensaje de saludo personalizado
+print(mensaje)  # Mostrar el mensaje de saludo
